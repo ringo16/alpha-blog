9d2c86e043dfa264ef43a5398104c0a9c2235dd7
Ryan Diep <ryandiep@live.com>
2017-01-24 23:58:13 -0800

    Add sign in upon sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
     # return @current_user if exist 
     # else
     # return user if session[:user_id] exist
-    @current_user ||=User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def logged_in?
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      # setting session[:user_id] means the user is logged in
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the alpha blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
